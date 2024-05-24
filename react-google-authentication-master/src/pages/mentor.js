d33affc1ddfd7d8732dab87fa663e73ada083de0
durga <bhavani@forensicalpha.co>
2024-05-24 16:54:24 +0530

    Frontend code team1

--- react-google-authentication-master/src/pages/mentor.js
+++ react-google-authentication-master/src/pages/mentor.js
@@ -23,15 +23,36 @@ function Mentor() {
             <Dropdown.Item as={Link} to="/studentScore">
               DAW
             </Dropdown.Item>
-            <Dropdown.Item onClick={() => handleOptionSelect("ss")}>
+            <Dropdown.Item >
               SS
             </Dropdown.Item>
-            <Dropdown.Item onClick={() => handleOptionSelect("it")}>
+            <Dropdown.Item >
               IT
             </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+  <input type="file" style={{ marginLeft:"8rem",marginTop:"2rem"}}  />
+  <button
+    type="button"
+    style={{
+      padding: "0.5rem",
+      fontSize: "1rem",
+      fontWeight: "bold",
+      textAlign: "center",
+      color: "#FFFFFF",
+      backgroundColor: "blue",
+      borderRadius: "0.75rem",
+        
+      margin:"2rem"
+    }}
+  >
+    Upload Marks
+  </button>
+</div>
+
+     
       <Logout />
     </div>
   );
--- react-google-authentication-master/src/pages/studentScore.js
+++ react-google-authentication-master/src/pages/studentScore.js
@@ -11,10 +11,10 @@ function StudentScores() {
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
+      try {     
         const response = await axios.get("http://127.0.0.1:5000/all-students-scores");
         setStudentData(response.data);
-        console.log(response.data); // Debugging statement
+        console.log(response.data); 
       } catch (error) {
         setError(error.message);
       } finally {
